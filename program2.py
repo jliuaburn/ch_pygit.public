2f76dc374a5d0fae523757e44ddacb802a1f7e54
PawelSz97 <psz@omg.pl>
2023-02-19 13:12:11 +0000

    add women 30+

--- program2.py
+++ program2.py
@@ -1,10 +1,18 @@
 #v.1.0 dzialajacej aplikacji
 wiek = input("Podaj wiek uzytkownika: ")
+
+plec = str(input("podaj swoja plec: "))
+
 #sprawdzenie czy wiek jest liczba calkowita
 if wiek.isdigit() == False:
     exit("Wiek musi byc liczba calkowita. zamykam apke")
 wiek=int(wiek)
-if wiek>=18 and wiek<=40:
+
+if wiek > 30 and plec == "kobieta" or "K" or "F" or "k" or "f":
+    print(" +30 to aperol spritz pierwszy gratis!")
+    print("mozesz kupowac alkohol")
+
+elif wiek>=18 and wiek<=40:
     print("witamy w apce. mozesz kupowac u nas alko")
 elif wiek>40:
     print("witamy w apce. mozesz kupowac u nas alkohol")
